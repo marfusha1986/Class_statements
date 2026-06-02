1b00d70ba7e32258196cfdab34893c6e033d4d3b
afaq imanova <afaq@imanov.name>
2026-06-02 11:46:32 +0300

    program.cs eklendi

---
+++ source/repos/ClassStatements/ClassStatements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bakiye bakiyeIslemi = new Bakiye();
+
+            decimal sonuc = bakiyeIslemi.Yekun(1500, 500);
+            Console.WriteLine("Kalan yekun: " + sonuc);
+        } 
+    }
+    class Bakiye
+    {
+        public decimal Y(decimal a, decimal b)
+        {
+            return a - b;
+        }
+        public decimal Yekun(decimal alacak, decimal borc)
+        {
+            return alacak - borc;
+        }
+        
+    }
+}
